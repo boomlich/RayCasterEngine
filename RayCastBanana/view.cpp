96bd43b72e619021642dd0a9cd4e3eef3f5ec802
boomlich <77412620+boomlich@users.noreply.github.com>
2022-07-27 05:39:59 +0200

    Initial commit

---
+++ RayCastBanana/view.cpp
@@ -0,0 +1,30 @@
+﻿#include "view.h"
+
+View::View(int width, int height) : window(sf::VideoMode(width, height), "Banana")
+{
+    this->width = width;
+    this->height = height;
+    render_loop();
+}
+
+
+/**
+ * \brief main rendering loop of the application
+ */
+void View::render_loop()
+{
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+	            
+                window.close();
+				
+        }
+
+        window.clear();
+        window.display();
+    }
+}
---
+++ RayCastBanana/view.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+
+class View
+{
+private:
+	sf::RenderWindow window;
+public:
+	int width;
+	int height;
+
+private:
+	void render_loop();
+
+public:
+	View(int width, int height);
+};
